e63b2d4ad092720a33bbb4fbfb6dd8c9b14bbd82
Alex M <safetyforced@gmail.com>
2016-07-26 00:18:26 -0400

    apres example 10

---
+++ ex10.py
@@ -0,0 +1,14 @@
+from random import randint
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+A list:\n\t* Cat Food\n\t* Fish\n\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ ex9.py
@@ -0,0 +1,16 @@
+# am I supposed to copy his comments?
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" # \n is the line break, neat
+
+print "Days is: ", days # what happened to our modulus?
+print "Months are: ", months
+
+print """
+So I guess this
+is how you do multiple line # haha, this doesn't work as a comment
+strings.  That's pretty
+neat, I guess.
+"""
+
+#I didn't screw up this time.  Ominous.
